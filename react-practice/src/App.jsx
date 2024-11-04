67c7dfcc52aef1cab1d78f095688b101a79fc626
atharsh07 <atharsh0425@gmail.com>
2024-11-04 22:27:29 +0530

    useState

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css'
-
+import { useState } from "react";
 function App() {
-
   return (
     <div>
         <b>
-        hi there 
+        this is an counter
         </b>
+        <Counter></Counter>
     </div>
   )
+}
+function Counter(params) {
+    const [count, setCount] = useState(0);
+    function increaseCounter() {
+        setCount(count + 1);
+    }
+    function decreaseCounter() {
+        setCount(count - 1);
+    }
+    function resetCounter() {
+        setCount(count - count);
+    }
+    return(
+        <div>
+            <h1 id='num'>{count}</h1>
+            <button onClick={increaseCounter}>Increase Counter</button>
+            <button onClick={decreaseCounter}>Decrase Counter</button>
+            <button onClick={resetCounter}>Reset Counter</button>
+        </div>
+    )
 }
 
 export default App
