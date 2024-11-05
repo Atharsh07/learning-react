beb7b15f63703d569a506de6650bef7cbb054c28
atharsh07 <atharsh0425@gmail.com>
2024-11-05 20:07:06 +0530

    useEffect

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,34 +1,28 @@
 import './App.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div>
-        <b>
-        this is an counter
-        </b>
-        <Counter></Counter>
-    </div>
-  )
-}
-function Counter(params) {
-    const [count, setCount] = useState(0);
-    function increaseCounter() {
-        setCount(count + 1);
-    }
-    function decreaseCounter() {
-        setCount(count - 1);
-    }
-    function resetCounter() {
-        setCount(count - count);
-    }
     return(
         <div>
-            <h1 id='num'>{count}</h1>
-            <button onClick={increaseCounter}>Increase Counter</button>
-            <button onClick={decreaseCounter}>Decrase Counter</button>
-            <button onClick={resetCounter}>Reset Counter</button>
+            <Counter></Counter>
         </div>
     )
 }
-
+function Counter() {
+    const[count, setCount] = useState(0);
+    console.log("coutner");
+    useEffect(function () {
+        setInterval(() => {
+            setCount(function (params) {
+                return params + 1;
+            })
+        }, 1000);
+        console.log("mounted");
+        
+    }, [])
+    return(
+        <div>
+            <h1 id='clock'>{count}</h1>
+        </div>
+    )
+}
 export default App
