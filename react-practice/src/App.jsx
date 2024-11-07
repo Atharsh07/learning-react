5d49ba75872cd6b31fb493be30afda9cf94c5055
atharsh07 <atharsh0425@gmail.com>
2024-11-07 22:09:48 +0530

    dependency array

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,36 +1,46 @@
 import './App.css'
 import { useState, useEffect } from "react";
+
+//relearing clean up , useEffect learning about dependency array
 function App() {
-    let [CounterVisble, setCounterVisble] = useState(true);
-    useEffect(function () {
-        setInterval(() => {
-            setCounterVisble(c => !c)
-        }, 6000);
-    }, [])
+    const[count, setCount] = useState(0);
+    const[count2, setCount2] = useState(0);
+    function increase() {
+        setCount(c => c+1);
+    }
+    function decrease() {
+        setCount2(c => c-1);
+    }
     return(
         <div>
-            {CounterVisble && <Counter></Counter>}
+            <Counter count={count} count2={count2}/>
+            <button onClick={increase}>Increase count</button>
+            <button onClick={decrease}>Decrease count</button>
         </div>
     )
 }
-function Counter() {
-    const[count, setCount] = useState(0);
-    console.log("coutner");
-    useEffect(function () {
-        let clock = setInterval(() => {
-            console.log("INSIDE");
-            setCount(c => c + 1)
-        }, 1000);
-        console.log("mounted");
+
+function Counter(props) {
+
+    useEffect(function() {
+        console.log("Mouted");
         return function () {
             console.log("Unmounted");
-            clearInterval(clock)
         }
-    }, [])
+    }, []) // this runs on  every cycle 
+
+    useEffect(function(){
+        console.log("count increase")
+    },[props.count]);  // only logs when the this state changes
+    useEffect(function(){
+        console.log("count decrease");
+    }, [props.count2]);
     return(
         <div>
-            <h1 id='clock'>{count}</h1>
+                Counter1 {props.count} <br />
+                Counter2 {props.count2} <br />
         </div>
     )
 }
+
 export default App
