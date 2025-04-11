f65f376a7bb3824b482039a97d8fbcf0a6eaa07d
atharsh07 <atharsh0425@gmail.com>
2025-04-11 19:17:56 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import User from "./components/User.jsx";
+
+function App() {
+
+
+  return (
+      <main>
+        <User name='atharsh' age={10} />
+      </main>
+  )
+}
+
+export default App
