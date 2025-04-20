d2e77bb7ec45b8ae7ac2217b00d0ba00718347e6
atharsh07 <atharsh0425@gmail.com>
2025-04-20 14:34:50 +0530

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Fetch from "./components/Fetch.jsx";
+import React from 'react'
+import { useReducer } from 'react'
+
+const initialState = { count : 0 };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { ...state , count: state.count + 1 };
+        case 'decrement':
+            return { ...state,count: state.count - 1 };
+        case 'reset':
+            return { ...state , count: 0 };
+        default:
+            break;
+    }
+}
 
 const App = () => {
+    const [state,dispatch] =useReducer(reducer, initialState);
   return (
     <div>
-        <Fetch/>
+        <h1>Count: {state.count}</h1>
+        <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
